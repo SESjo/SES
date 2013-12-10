80e38f9950f17839f4c6d7a9baea39e05bb9779b
SESman <SESman@user.noreply.github.com>
2013-12-10 14:07:23 +0100

    Add new args in existsVars(): substring and ignorecase logicals.

--- R/SESutils.r
+++ R/SESutils.r
@@ -64,29 +64,39 @@ replaceMissing <- function(x, na.0=NaN, na.1=NA) {
 #' }
 #' fun(x) # Check the warnings
 findVars <- function(vars, obj, varnames=NULL, ...){
-	idx <- existsVars(vars, obj)
-	if (is.null(varnames)){varnames <- vars}
-	for (k in seq_along(idx)){
-		if (is.data.frame(obj)) {assign(varnames[k], obj[ , idx[k]], envir=sys.frame(-1), ...)}
-		else if (is.list(obj)) {assign(varnames[k], obj[[idx[k]]], envir=sys.frame(-1), ...)}
-	}
+  idx <- existsVars(vars, obj)
+  if (is.null(varnames)){varnames <- vars}
+  for (k in seq_along(idx)){
+    if (is.data.frame(obj)) {assign(varnames[k], obj[ , idx[k]], envir=sys.frame(-1), ...)}
+    else if (is.list(obj)) {assign(varnames[k], obj[[idx[k]]], envir=sys.frame(-1), ...)}
+  }
 }
 
 #' existVar
 #' @description Search for variables in an object. Throw an error if not when a variable is not found. Print a warning if partial matching only.
-#' @param vars The list of variable names.
+#' @param vars The list of variable names. Can be a regular expression.
 #' @param obj The object in which to search.
 #' @param idx Should the indexes of matching columns be returned ?
+#' @param substring If \code{FALSE} then check that \code{pattern} and matching variables have the same number of characters.
+#' @param ignore.case Should case variants of the pattern be investigated ?
+#' @param ... Other arguments to be passed to \code{grepl}. Set \code{fixed} to \code{TRUE} if  \code{vars} is to be matched as is (not a regular expression).
 #' @seealso \code{\link{findVars}}
 #' @author Yves
 #' @keywords internal
 #' @export
-existsVars <- function(vars, obj, idx=TRUE) {
+existsVars <- function(vars, obj, idx=TRUE, substring=TRUE, ignore.case=TRUE, ...) {
   col.idx <- rep(NA, length(vars))
-  col <- sapply(vars, grepl, x=names(obj))
+  col <- sapply(vars, grepl, x=names(obj), ...)
+  if (!substring){
+    for (j in 1:ncol(col)){
+      n <- rep(nchar(vars[j]), nrow(col))
+      n[which(col[ , j])] <- nchar(names(obj)[which(col[ , j])])
+      col[n != nchar(vars[j]), j] <- FALSE
+    }
+  }
   obj.name <- as.expression(substitute(obj))
   for (j in 1:ncol(col)){
-    if (!any(col[, j])){
+    if (!any(col[, j]) & ignore.case){
       jlower <- grepl(tolower(vars[j]), tolower(names(obj)))
       if (any(jlower)) {
         col.idx[j] <- ifelse(sum(jlower) == 1, which(jlower), NA)
@@ -104,9 +114,15 @@ existsVars <- function(vars, obj, idx=TRUE) {
       }
     } else {
       if (length(which(col[, j])) > 1) {
-        warning(paste0(paste("Several variables of", obj.name, "matched with", vars[j]), ": ", names(obj)[which(col[ , j])]))
+        warning(paste0(paste("Several variables of", obj.name, "matched with", vars[j], ": "),
+                       paste(names(obj)[which(col[ , j])], collapse=", "),
+                       "Try with 'substring=FALSE'."))
+        col.idx[j] <- NA
+      }else if (length(which(col[, j])) == 0){
+        col.idx[j] <- NA
+      }else{
+        col.idx[j] <- which(col[ , j])
       }
-      col.idx[j] <- which(col[ , j])
     }
   }
   if (any(is.na(col.idx))){
