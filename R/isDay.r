8c66f4e8e69268cf880c11a2822bb803f552f628
SESman <yves-lb@hotmail.fr>
2013-12-15 19:22:07 +0100

    isDay() robust to colnames changes by user

--- R/isDay.r
+++ R/isDay.r
@@ -18,7 +18,8 @@
 #' isDay(convertTime(time, to="posx"), testPts)
 isDay <- function(Time, loc, stat=NULL, elevlim=c(-18, 18), append=TRUE) {
 	if (!is.null(stat)){
-    findVars(c("Time", "Lat", "Lon"), stat)
+		statVars <- userHeader(c("Time", "Lat", "Lon"), type="stat")
+		findVars(statVars, stat, varnames=c("Time", "Lat", "Lon"))
 		loc <- data.frame(Lat=Lat, Lon=Lon)
 	}
 	
@@ -26,12 +27,12 @@ isDay <- function(Time, loc, stat=NULL, elevlim=c(-18, 18), append=TRUE) {
 	if (any(is.na(loc))) {
 		locNA <- apply(is.na(loc), 1, sum) != 0
 		sunAngle <- try(rep(NA, nrow(Time)), silent=TRUE)
-    if (is.error(sunAngle)) sunAngle <- rep(NA, length(Time))
+		if (is.error(sunAngle)) sunAngle <- rep(NA, length(Time))
 		sunAngle[!locNA] <- sunPosition(time=Time[!locNA], loc=loc[!locNA, ])$el
 	}
 	else {
-	  locNA <- try(rep(FALSE, nrow(Time)), silent=TRUE)
-	  if (is.error(locNA)) locNA <- rep(FALSE, length(Time))
+		locNA <- try(rep(FALSE, nrow(Time)), silent=TRUE)
+		if (is.error(locNA)) locNA <- rep(FALSE, length(Time))
 		sunAngle <- sunPosition(time=Time, loc=loc)$el
 	}
 	is.Day <- rep(NA, length(locNA))
