bdb55f45627e78a95c996f1a1f05ea5446fb326e
SESman <yveslebras.fr@gmail.fr>
2014-04-03 19:47:00 +0200

    add anaBehav

--- R/function_matlab_translated.r
+++ R/function_matlab_translated.r
@@ -13,7 +13,7 @@
 #' indice of period end, type of period, duration (s), the dive number, 
 #' indice of bottom start, indice of bottom end, bottom duration.
 #' @details See Yves Le Bras M1 report for details about the method.
-#' @seealso \code{\link{divesID}}, \code{\link{poly4delim}}
+#' @seealso \code{\link{divesID}}, \code{\link{bottomDelim}}, \code{\link{anaBehav}}.
 #' @export
 #' @examples
 #' path <- system.file("extdata", package="SES")
@@ -29,7 +29,9 @@
 #' points(-Depth ~ Time, df, type = 'p', col='red', pch = 19)
 anaDives <- function(tdr, dpthThres = 15, durThres = 300, 
                      spdThres = .75, w = 12)
-  poly4delim(tdr, divesID(tdr))
+  bottomDelim(tdr, divesID(tdr))
+
+
 
 #' Find the dives from TDR data
 #' 
@@ -38,7 +40,7 @@ anaDives <- function(tdr, dpthThres = 15, durThres = 300,
 #' @inheritParams anaDives
 #' @return A data frame with the following variable: indice of period start, 
 #' indice of period end, type of period, duration (s), the dive number.
-#' @seealso \code{\link{poly4delim}}, \code{\link{anaDives}}
+#' @seealso \code{\link{bottomDelim}}, \code{\link{anaDives}}
 #' @export
 divesID <- function(tdr, durThres = 300, dpthThres = 15){
   
@@ -63,15 +65,12 @@ divesID <- function(tdr, durThres = 300, dpthThres = 15){
   dvs$dive <- rep(0, nrow(dvs))
   dvs$dive[dvs$value == 'Diving'] <- seq_along(dvs$dive[dvs$value == 'Diving'])
   
-  #   if (append) # Propagate dive number to tdr data
-  #     tdr$Dive.id <- rep(dvs$dive, dvs$length) 
-  
   # Convert length into seconds
   dvs$length <- dvs$length * reso
   
   # Supress last line if not a surface
   dvs <- if (dvs$value[nrow(dvs)] == 'Diving'){dvs[-nrow(dvs), ]}
-  names(dvs) <- c("st.idx", "ed.idx", "type", "duration", "diveNumb")
+  names(dvs) <- c("st.idx", "ed.idx", "type", "Dive.dur", "Dive.numb")
   dvs
 }
 
@@ -87,7 +86,7 @@ divesID <- function(tdr, durThres = 300, dpthThres = 15){
 #' @details See Yves Le Bras M1 report for details about the method.
 #' @seealso \code{\link{divesID}}, \code{\link{anaDives}}
 #' @export
-poly4delim <- function(tdr, dvs, spdThres = .75, w = 12) {
+bottomDelim <- function(tdr, dvs, spdThres = .75, w = 12) {
   
   findDefaultVars(c('Time', 'Depth'), tdr, type.obj='tdr')
   dTime <- as.numeric(diff(Time))
@@ -97,18 +96,74 @@ poly4delim <- function(tdr, dvs, spdThres = .75, w = 12) {
   spd <- rollapply(diff(Depth) / dTime, mean, w)
   
   # Delim bottom
-  bbtNames <- c("btt.st.idx", "btt.ed.idx", "btt.duration")
+  bbtNames <- c("btt.st.idx", "btt.ed.idx", "Btt.dur")
   dvs[ , bbtNames] <- rep(NA, nrow(dvs))
-  for (ii in seq_along(dvs$dive)){
+  for (ii in seq_along(dvs$"Dive.numb")){
     if (dvs$type[ii] == 'Surface') next
     y <- spd[dvs$st.idx[ii]:dvs$ed.idx[ii]]
     x <- Time[dvs$st.idx[ii]:dvs$ed.idx[ii]]
-    spdMod <- lm(y ~ poly(x, degree=4))
+    spdMod <- lm(y ~ poly(x, degree = 4))
     btt <- per(abs(predict(spdMod)) < spdThres, idx = TRUE)
     dvs[ii, bbtNames] <- unlist(btt[btt$value, -3]) + c(rep(dvs[ii, 1] - 1, 2), 0)
   }
-  dvs$btt.duration <- dvs$btt.duration * reso
-  names(dvs) <- c("st.idx", "ed.idx", "type", "duration", "diveNumb", "btt.st.idx", 
-                  "btt.ed.idx", "btt.duration")
+  dvs$Btt.dur <- dvs$Btt.dur * reso
+  names(dvs) <- c("st.idx", "ed.idx", "type", "Dive.dur", "Dive.numb", "btt.st.idx", 
+                  "btt.ed.idx", "Btt.dur")
   return(dvs)
+}
+
+#' Apply a function to each dive/surface/bottom
+#' 
+#' \code{dvapply} is a utility to apply function to specific parts of a TDR dataset.
+#' 
+#' @param FUN Function to apply
+#' @param obj A TDR object
+#' @param dvs Optional. A table with dives/surfaces/bootoms indices as returned by 
+#' \code{\link{divesID}} or \code{\link{anaDives}}.
+#' @param type The periods involved: to choose in \code{c('dive', 'surface', 
+#' 'both', 'bottom')}.
+#' @param ... Other arguments to be passed to \code{FUN}.
+#' @export
+#' @examples
+#' path <- system.file("extdata", package="SES")
+#' pathname <- file.path(path, "2011-16_SES_example_accelero.mat")
+#' ses <- importSES(pathname)
+#' depth.max <- dvapply(function(tdr){max(tdr$Depth)}, ses$tdr)
+dvapply <- function(FUN, obj, dvs, 
+                    type = c('dive', 'surface', 'both', 'bottom', 'ascent', 'descent'), ...){
+  if (missing(dvs))
+    dvs <- switch(match.arg(type), bottom = anaDives(obj), divesID(obj))
+  idx <- switch(match.arg(type),
+                dive = dvs[dvs$type == 'Diving', 1:2],
+                surface = dvs[dvs$type == 'Surface', 1:2],
+                both = dvs[ , 1:2], 
+                bottom = dvs[dvs$type == 'Diving', 6:7])
+  f <- function(st, ed, x, ...) FUN(x[st:ed, ], ...)
+  args <- if (is.null(list(...))) list(x = obj) else c(list(x = obj), list(...))
+  mapply(f, st = idx[ , 1], ed = idx[ , 2], MoreArgs = args)
+}
+
+#' Compute behavioral variables from TDR dataset
+#' 
+#' @param tdr The TDR dataset.
+#' @param dvs Optional. A table with dives/surfaces/bootoms indices as returned by 
+#' \code{\link{divesID}} or \code{\link{anaDives}}.
+anaBehav <- function(tdr, dvs){
+  
+  if (missing(dvs))
+    dvs <- anaDives(tdr)
+  
+  dvs <- split(dvs, dvs$type)
+  dvsVars <- c("Dive.dur", "Dive.numb", "Btt.dur")
+  # List of functions to use on the entire dive
+  funsD <- list(Depth.max = function(tdr){max(tdr$Depth)},
+                Catch.numb = function(tdr){cch <- per(tdr$is.Catch) ; nrow(cch[cch[,1],])})
+  # List of functions to use on on the bottom
+  funsB <- list(SinuV = function(tdr){var(tdr$Depth)})
+  
+  stats <- lapply(funsD, dvapply, obj = tdr, dvs = dvs$Diving)
+  stats <- c(stats, lapply(funsB, dvapply, obj = tdr, dvs = dvs$Diving, type = 'bottom'))
+  stats[dvsVars] <- dvs$Diving[ , dvsVars]
+  stats$Surf.dur.aft <- dvs$Surface$Dive.dur[-1]
+  as.data.frame(stats)
 }
