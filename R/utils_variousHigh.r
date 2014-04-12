c6ea3f693ca48d12f974b61bb6f182f193d7dfd2
SESman <yves-lb@hotmail.fr>
2014-04-12 10:55:59 +0200

    Fix type = 'both' issue of dvapply()

--- R/utils_variousHigh.r
+++ R/utils_variousHigh.r
@@ -13,28 +13,29 @@
 #' @seealso \code{\link{anaDives}}
 #' @export
 randomDv <- function(obj, dvs, plt = TRUE, n = NULL, btt = TRUE){
-  if (is.ses(obj)){
-    obj <- obj$tdr
-    if (missing(dvs)) dvs <- obj$stat
-  }
-  n <- n %else% sample(setdiff(unique(dvs$Dive.id), 0), 1)
-  has.Btt <- partial(any %.% grepl, pattern = '^b.*x$')
-  if (btt && has.Btt(names(dvs))){
-    while (is.na(dvs$btt.st.idx[dvs$Dive.id == n]))
-      n <- sample(setdiff(unique(dvs$Dive.id), 0), 1)
-  }
-  df <- function(cols)
-    obj[do.call(seq, unname(dvs[dvs$Dive.id == n, grepl(cols, names(dvs))])), ]
-  if (plt){
-    plot(-Depth ~ Time, df('^[^b].*x$'), type = 'l', lwd = 2, main = paste('Dive number', n))
-    abline(h = 0, col = 'gray', lty = 2)
-    if (btt && has.Btt(names(dvs)))
-      points(-Depth ~ Time, df('^b.*x$'), type = 'l', col='red', lwd = 2)
-  }
-  if (btt)
-    invisible(list(n = n, dv = df('^[^b].*x$'), btt = df('^b.*x$')))
-  else
-    invisible(list(n = n, dv = df('^[^b].*x$')))
+	if (missing(dvs)) 
+		dvs <- divesID(obj)
+	if (is.ses(obj)){
+		obj <- obj$tdr
+	} 
+	n <- n %else% sample(setdiff(unique(dvs$Dive.id), 0), 1)
+	has.Btt <- partial(any %.% grepl, pattern = '^b.*x$')
+	if (btt && has.Btt(names(dvs))){
+		while (is.na(dvs$btt.st.idx[dvs$Dive.id == n]))
+			n <- sample(setdiff(unique(dvs$Dive.id), 0), 1)
+	}
+	df <- function(cols)
+		obj[do.call(seq, unname(dvs[dvs$Dive.id == n, grepl(cols, names(dvs))])), ]
+	if (plt){
+		plot(-Depth ~ Time, df('^[^b].*x$'), type = 'l', lwd = 2, main = paste('Dive number', n))
+		abline(h = 0, col = 'gray', lty = 2)
+		if (btt && has.Btt(names(dvs)))
+			points(-Depth ~ Time, df('^b.*x$'), type = 'l', col='red', lwd = 2)
+	}
+	if (btt)
+		invisible(list(n = n, dv = df('^[^b].*x$'), btt = df('^b.*x$')))
+	else
+		invisible(list(n = n, dv = df('^[^b].*x$')))
 }
 
 
@@ -58,21 +59,21 @@ randomDv <- function(obj, dvs, plt = TRUE, n = NULL, btt = TRUE){
 #' lines(rollapply(x, median, 5), col = "blue", lwd = 2)
 #' lines(rollapply(x, mean, 5, wty='f'), col = "green", lwd = 2)
 rollapply <- function(x, FUN, w = 10, wty = 'm', ...) {
-  out <- rep(NA, length(x))
-  offset <- trunc(w / 2)
-  if (wty == 'm'){
-    for (i in (offset + 1):(length(x) - w + offset + 1)) {
-      out[i] <- FUN(x[(i - offset):(i + offset)], ...)
-    }
-  } else if (wty == 'f'){
-    for (i in seq(offset + 1, length(x) - w + offset + 1, by = w)) {
-      out[(i - offset):(i + offset)] <- rep(FUN(x[(i - offset):(i + offset)], ...), 
-                                            2*offset + 1)
-    }
-  } else {
-    stop('Unknown window type specified.')
-  }
-  out
+	out <- rep(NA, length(x))
+	offset <- trunc(w / 2)
+	if (wty == 'm'){
+		for (i in (offset + 1):(length(x) - w + offset + 1)) {
+			out[i] <- FUN(x[(i - offset):(i + offset)], ...)
+		}
+	} else if (wty == 'f'){
+		for (i in seq(offset + 1, length(x) - w + offset + 1, by = w)) {
+			out[(i - offset):(i + offset)] <- rep(FUN(x[(i - offset):(i + offset)], ...), 
+												  2*offset + 1)
+		}
+	} else {
+		stop('Unknown window type specified.')
+	}
+	out
 }
 
 #' Use time and location to find if events occured during the day or the night
@@ -96,36 +97,36 @@ rollapply <- function(x, FUN, w = 10, wty = 'm', ...) {
 #' isDay(convertTime(time, to="posx"), testPts)
 isDay <- function (time, loc, stat = NULL, elevlim = c(-18, 18), append = TRUE) 
 {
-  if (!is.null(stat)) {
-    findDefaultVars(c("Time", "Lat", "Lon"), stat, type.obj = "stat")
-    if (missing(time)) time <- Time
-    if (missing(loc))  loc  <- data.frame(Lat = Lat, Lon = Lon)
-  }
-  if (any(is.na(time))) 
-    stop("NA not allowed in 'time' argument")
-  if (any(is.na(loc))) {
-    locNA <- apply(is.na(loc), 1, sum) != 0
-    sunAngle <- try(rep(NA, nrow(time)), silent = TRUE)
-    if (inherits(sunAngle, "try-error")) 
-      sunAngle <- rep(NA, length(time))
-    sunAngle[!locNA] <- sunPosition(time = time[!locNA], 
-                                    loc = loc[!locNA, ])$el
-  }
-  else {
-    locNA <- try(rep(FALSE, nrow(time)), silent = TRUE)
-    if (inherits(locNA, "try-error")) 
-      locNA <- rep(FALSE, length(time))
-    sunAngle <- sunPosition(time = time, loc = loc)$el
-  }
-  is.Day <- rep(NA, length(locNA))
-  is.Day[!locNA & sunAngle > elevlim[2]] <- TRUE
-  if (!is.null(stat) & append) {
-    stat$is.Day <- is.Day
-    return(stat)
-  }
-  else {
-    return(is.Day)
-  }
+	if (!is.null(stat)) {
+		findDefaultVars(c("Time", "Lat", "Lon"), stat, type.obj = "stat")
+		if (missing(time)) time <- Time
+		if (missing(loc))  loc  <- data.frame(Lat = Lat, Lon = Lon)
+	}
+	if (any(is.na(time))) 
+		stop("NA not allowed in 'time' argument")
+	if (any(is.na(loc))) {
+		locNA <- apply(is.na(loc), 1, sum) != 0
+		sunAngle <- try(rep(NA, nrow(time)), silent = TRUE)
+		if (inherits(sunAngle, "try-error")) 
+			sunAngle <- rep(NA, length(time))
+		sunAngle[!locNA] <- sunPosition(time = time[!locNA], 
+										loc = loc[!locNA, ])$el
+	}
+	else {
+		locNA <- try(rep(FALSE, nrow(time)), silent = TRUE)
+		if (inherits(locNA, "try-error")) 
+			locNA <- rep(FALSE, length(time))
+		sunAngle <- sunPosition(time = time, loc = loc)$el
+	}
+	is.Day <- rep(NA, length(locNA))
+	is.Day[!locNA & sunAngle > elevlim[2]] <- TRUE
+	if (!is.null(stat) & append) {
+		stat$is.Day <- is.Day
+		return(stat)
+	}
+	else {
+		return(is.Day)
+	}
 }
 
 #' Copy values of a variable from an table to another within a 'ses' object.
@@ -138,34 +139,34 @@ isDay <- function (time, loc, stat = NULL, elevlim = c(-18, 18), append = TRUE)
 #' @seealso \code{\link{addLoc}}
 #' @export
 addVar <- function(var, from, to, ses=NULL, append=TRUE){
-  if (!is.null(ses)) {
-    from <- eval(parse(text=paste0(substitute(ses), '$', from)))
-    to <- eval(parse(text=paste0(substitute(ses), '$', to)))
-  }
-  argtdr <- match("tdr", setdiff(c(class(from), class(to)), "data.frame"), nomatch=0)
-  
-  dvidFrom <- userHeader("Dive.id", type=class(from)[1])
-  findVars(dvidFrom, from, type="check")
-  dvidTo <- userHeader("Dive.id", type=class(to)[1])
-  findVars(dvidTo, to, type="check")
-  
-  if (argtdr == 1){
-    to <- merge(to, unique(from[ , c(dvidFrom,  var)]), by.x=dvidTo, by.y=dvidFrom)
-    if (!append) return(to[, var])
-    class(to) <- c("statdives", "data.frame")
-  } else if (argtdr == 2){
-    if (any(is.na(from[ , var]))) {
-      message("Missing values (NAs) pasted as zeros")
-      from[ , var] <- replaceMissing(from[ , var], na.0=NA, na.1=0)
-    }
-    dvs <- per(to[ , dvidTo])
-    dvs$value[dvs$value != 0] <- from[ , var] 
-    if (!append) return(rep(dvs$value, dvs$length))
-    to[ , var] <- rep(dvs$value, dvs$length)
-  }else{
-    stop("Input objects must be of class 'tdr', and 'statdives'.")
-  }
-  return(to)
+	if (!is.null(ses)) {
+		from <- eval(parse(text=paste0(substitute(ses), '$', from)))
+		to <- eval(parse(text=paste0(substitute(ses), '$', to)))
+	}
+	argtdr <- match("tdr", setdiff(c(class(from), class(to)), "data.frame"), nomatch=0)
+	
+	dvidFrom <- userHeader("Dive.id", type=class(from)[1])
+	findVars(dvidFrom, from, type="check")
+	dvidTo <- userHeader("Dive.id", type=class(to)[1])
+	findVars(dvidTo, to, type="check")
+	
+	if (argtdr == 1){
+		to <- merge(to, unique(from[ , c(dvidFrom,  var)]), by.x=dvidTo, by.y=dvidFrom)
+		if (!append) return(to[, var])
+		class(to) <- c("statdives", "data.frame")
+	} else if (argtdr == 2){
+		if (any(is.na(from[ , var]))) {
+			message("Missing values (NAs) pasted as zeros")
+			from[ , var] <- replaceMissing(from[ , var], na.0=NA, na.1=0)
+		}
+		dvs <- per(to[ , dvidTo])
+		dvs$value[dvs$value != 0] <- from[ , var] 
+		if (!append) return(rep(dvs$value, dvs$length))
+		to[ , var] <- rep(dvs$value, dvs$length)
+	}else{
+		stop("Input objects must be of class 'tdr', and 'statdives'.")
+	}
+	return(to)
 }
 
 #' Copy locations from one table to another within a 'ses' object
@@ -185,16 +186,16 @@ addVar <- function(var, from, to, ses=NULL, append=TRUE){
 #' ses$tdr <- addLoc(ses$stat, ses$tdr)
 #' head(ses$tdr)
 addLoc <- function(from, to, ses=NULL, append=TRUE){
-  if (!is.null(ses)) {
-    from <- eval(parse(text=paste0(substitute(ses), '$', from)))
-    to <- eval(parse(text=paste0(substitute(ses), '$', to)))
-  }
-  
-  vars <- c("Lat", "Lon")
-  findDefaultVars(vars, from, type.obj=class(from)[1], type="check")
-  if (!append) return(lapply(vars, addVar, from, to, append=FALSE))
-  to[ , vars] <- lapply(vars, addVar, from, to, append=FALSE)
-  return(to)
+	if (!is.null(ses)) {
+		from <- eval(parse(text=paste0(substitute(ses), '$', from)))
+		to <- eval(parse(text=paste0(substitute(ses), '$', to)))
+	}
+	
+	vars <- c("Lat", "Lon")
+	findDefaultVars(vars, from, type.obj=class(from)[1], type="check")
+	if (!append) return(lapply(vars, addVar, from, to, append=FALSE))
+	to[ , vars] <- lapply(vars, addVar, from, to, append=FALSE)
+	return(to)
 }
 
 #' Decompose an atomic vector to its successive values and their length.
@@ -218,15 +219,15 @@ addLoc <- function(from, to, ses=NULL, append=TRUE){
 #' # Because characters are not converted to factors
 #' # inherits(y$value, class(x))            # TRUE
 per <- function(x, idx=FALSE) {
-  # Save original object
-  x.org <- x
-  # Concert x to numeric
-  if      (is.logical(x))   {x <- as.numeric(x)}
-  else if (is.factor(x))    {x <- as.numeric(x)}
-  else if (is.character(x)) {x <- as.numeric(as.factor(x))}
-  # Compute and return
-  chg <- diff(x, lag=1)
-  end <- c(which(chg != 0), length(x)) ; start <- c(1, end[-length(end)] + 1)
-  if (idx) return(data.frame(st.idx=start, ed.idx=end, value=x.org[start], length=end - start + 1, stringsAsFactors=FALSE))
-  else return(data.frame(value=x.org[start], length=end - start + 1, stringsAsFactors=FALSE))
+	# Save original object
+	x.org <- x
+	# Concert x to numeric
+	if      (is.logical(x))   {x <- as.numeric(x)}
+	else if (is.factor(x))    {x <- as.numeric(x)}
+	else if (is.character(x)) {x <- as.numeric(as.factor(x))}
+	# Compute and return
+	chg <- diff(x, lag=1)
+	end <- c(which(chg != 0), length(x)) ; start <- c(1, end[-length(end)] + 1)
+	if (idx) return(data.frame(st.idx=start, ed.idx=end, value=x.org[start], length=end - start + 1, stringsAsFactors=FALSE))
+	else return(data.frame(value=x.org[start], length=end - start + 1, stringsAsFactors=FALSE))
 }
